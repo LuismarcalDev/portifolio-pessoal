9b0f99db3c33b295ea4f0507c2dafcf012f83cff
Luis Felippe <115747500+LuismarcalDev@users.noreply.github.com>
2024-07-29 23:34:42 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,39 @@
+ScrollReveal().reveal(".left", {
+  origin: "left",
+  duration: 2000,
+  distance: "5%",
+});
+
+ScrollReveal().reveal(".right", {
+  origin: "right",
+  duration: 2000,
+  distance: "5%",
+});
+
+ScrollReveal().reveal(".sobre-mim-geral", {
+  origin: "top",
+  duration: 2000,
+  distance: "5%",
+});
+
+ScrollReveal().reveal(".habilidades-geral", {
+  origin: "top",
+  duration: 2000,
+  distance: "5%",
+});
+ScrollReveal().reveal(".projetos-geral", {
+  origin: "left",
+  duration: 2000,
+  distance: "5%",
+});
+
+ScrollReveal().reveal(".contato-geral", {
+  origin: "top",
+  duration: 2000,
+  distance: "5%",
+});
+ScrollReveal().reveal("footer", {
+  origin: "top",
+  duration: 2000,
+  distance: "5%",
+});
